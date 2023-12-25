477e9dcdbb5bf2a41ff76c5625980f575dba53d2
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-25 12:05:52 +0530

    Update stg_facebook_ads__basic_ad.sql

--- models/stg_facebook_ads__basic_ad.sql
+++ models/stg_facebook_ads__basic_ad.sql
@@ -1,4 +1,11 @@
-{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True)) }}
+{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True),
+     unique_key = ['source_relation','date_day','ad_id','account_id'],
+    partition_by={
+      "field": "date_day", 
+      "data_type": "date",
+      "granularity": "day"
+    }
+    ) }}
 
 with base as (
 
