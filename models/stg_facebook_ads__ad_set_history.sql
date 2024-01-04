9bfee197683ae5eb4d4f1334848a2f9de4af50ae
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-01-04 22:05:43 +0530

    Update stg_facebook_ads__ad_set_history.sql

--- models/stg_facebook_ads__ad_set_history.sql
+++ models/stg_facebook_ads__ad_set_history.sql
@@ -36,13 +36,13 @@ final as (
 
     select
         source_relation, 
-        updated_time as updated_at,
+        CAST(FORMAT_TIMESTAMP("%F %T", updated_time, "America/New_York") AS TIMESTAMP) as updated_at,    --EST Conversion
         cast(id as {{ dbt.type_bigint() }}) as ad_set_id,
         name as ad_set_name,
         cast(account_id as {{ dbt.type_bigint() }}) as account_id,
         cast(campaign_id as {{ dbt.type_bigint() }}) as campaign_id,
-        start_time as start_at,
-        end_time as end_at,
+        CAST(FORMAT_TIMESTAMP("%F %T", start_time, "America/New_York") AS TIMESTAMP)as start_at,    --EST Conversion
+        CAST(FORMAT_TIMESTAMP("%F %T", end_time, "America/New_York") AS TIMESTAMP) as end_at,    --EST Conversion 
         bid_strategy,
         daily_budget,
         budget_remaining,
